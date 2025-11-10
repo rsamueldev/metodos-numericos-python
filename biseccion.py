a3a195d1a5fc1e30c92b9030aa2ed10e3e789a4e
Samuel Rosales <samuelalejandro498@gmaail.com>
2025-11-10 18:59:06 -0400

    Arreglo errores de almacenamiento y calculo de error y punto medio en biseccion.py, y corrijo detalles de visualizacion de la consola

--- biseccion.py
+++ biseccion.py
@@ -1,26 +1,75 @@
 import sympy as sp
+import os
 
-def biseccion(expr_str, a, b, err):
+def biseccion(expr_str, a, b, err_relativo):
     #Definiciones basicas
     x = sp.Symbol('x')  #Definicion de variable x
     f = sp.sympify(expr_str)  #Transforma la expresion a una evaluable
     f_num = sp.lambdify(x, f)  #Funcion para evaluar valores reemplazando x
 
-    #Comprobacion de restricciones del metodo de biseccion
+    #RESTRICCIONES
     #1. Que la funcion sea continua
-    a = 0 #Punto de partida para evaluar continuidad
+    p = (a + b)/2 #Punto de partida para evaluar continuidad
 
     #Calculamos los limites laterales y el valor en el punto
-    lim_izq = sp.limit(f, x, a, dir='-')
-    lim_der = sp.limit(f, x, a, dir='+')
-    valor = f.subs(x, a)    
+    lim_izq = sp.limit(f, x, p, dir='-')
+    lim_der = sp.limit(f, x, p, dir='+')
+    valor = f.subs(x, p)    
     
     # Verificación de continuidad
     if lim_izq == lim_der == valor:
-        print(f"La función es continua en x = {a}")
+        os.system("cls") #Limpia pantalla
+        print(f"La función es continua en x = {p}")
     else:
-        print(f"La función NO es continua en x = {a}")
+        print(f"La función NO es continua en x = {p}")
 
     #2. Que tengan signos diferentes
     if f_num(a) * f_num(b) > 0:
-        print("El intervalo no es valido. Deben tener signos opuestos")
+        print("El intervalo no es valido. Deben tener signos opuestos.")
+    else:
+        print("El intervalo tiene signos opuestos.")
+    iteraciones = 0
+    input("\nPresiona Enter para continuar...") #Congela consola
+
+    os.system("cls") #Limpia pantalla
+
+    # Encabezado de tabla
+    print("Método de Bisección")
+    print(f"Función: f(x) = {expr_str}")
+    print(f"Intervalo inicial: [{a}, {b}]\n")
+    print("{:<10} {:<10} {:<10} {:<10} {:<12} {:<12} {:<12} {:<10}".format(
+        "Iter", "a", "b", "m", "f(a)", "f(b)", "f(m)", "Error"))
+    print("-"*85)
+
+    #ITERACIONES
+    err = None
+    
+    while True:
+
+        #Calculo del punto medio
+        puntoMedio = (a + b)/2
+
+        # Mostrar datos en tabla
+        print("{:<10} {:<10.5f} {:<10.5f} {:<10.5f} {:<12.5f} {:<12.5f} {:<12.5f} {:<10}".format(
+            iteraciones, a, b, puntoMedio, f_num(a), f_num(b), f_num(puntoMedio), f"{err:.5f}" if err else "-"))
+
+
+        #Calcular los nuevos intervalos 
+        if f_num(a) * f_num(puntoMedio) < 0:
+            b = puntoMedio
+        elif f_num(puntoMedio) * f_num(b):
+            a = puntoMedio
+
+        #Verificamos si lleva mas de una iteracion para calcular el error
+        if iteraciones > 0:
+            err = abs((puntoMedio - puntoMedioAnt)/puntoMedio)
+            print(puntoMedio, puntoMedioAnt)
+            if err < err_relativo:
+                break
+
+        #Fin del ciclo
+        #Contador de ciclos
+        iteraciones += 1
+
+        #Punto medio actual se guarda para cuando se reinicie el ciclo
+        puntoMedioAnt = puntoMedio 
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+from biseccion import biseccion
+import os
+
 def main():
     #Estructura del menu
     print(".:APROXIMACION DE RAICES:.:\n")
     expr_str = input("Ingrese la funcion f(x) = ")
 
+    os.system("cls") #Limpia pantalla
+
     #Seleccion de metodo
     print("Seleccione el metodo a utilizar:")
-    op = input("1. Biseccion\n2. Newton-Raphson: ")
+    op = input("1. Biseccion\n2. Newton-Raphson\nOpcion: ")
 
     while True:
+        os.system("cls") #Limpia pantalla
         if op == '1':
             a = float(input("Ingresa el intervalo a: "))
             b = float(input("Ingresa el intervalo b: "))
-            err = float(input("Ingrese el error relativo: "))
-            #Funcion de biseccion a crear
+            err_relativo = float(input("Ingrese el error relativo: "))
+            biseccion(expr_str, a, b, err_relativo)
             break
         elif op == '2':
+            os.system("cls") #Limpia pantalla
             x0 = float(input("Ingresa el valor incial de x0: "))
             err = float(input("Ingrese el error relativo: "))
             #Funcion de newton-raphson a crear
